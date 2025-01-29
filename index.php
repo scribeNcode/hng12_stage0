9fbb57f6aee1bf35bd372b39abc96e1e26a1f658
scribeNcode <deyeye99@gmail.com>
2025-01-29 16:07:46 +0100

    create current date variable

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// set cores 
+// header("Access-Control-Allow-Origin : *")
+// header("Content-Type : application/json")
+
+//GET CURRENT DATE
+
+$current_date = gmdate("Y-m-d\TH:i:s\z");
+
+echo $current_date
+
+?>
